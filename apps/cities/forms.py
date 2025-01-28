b9a0d16282203772dc4e9cb71bb25f7367d2573a
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-01-28 23:28:00 +0200

    working on autocomplete field

--- apps/cities/forms.py
+++ apps/cities/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
+from apps.core.widgets import get_autocomplete_field
+
 from .models import City
 
 
 class CityForm(forms.ModelForm):
+    governorate = get_autocomplete_field(
+        app_label="governorates",
+        model_name="Governorate",
+        object_name="governorate",
+        field_name="name",
+    )
 
     class Meta:
         model = City
--- apps/core/widgets.py
+++ apps/core/widgets.py
@@ -1,26 +1,45 @@
-from django.forms.widgets import SelectMultiple
+import json
+
+from django import forms
+from django.forms.widgets import SelectMultiple, TextInput
 from django.template.loader import render_to_string
 
 
-class ComboboxWidget(SelectMultiple):
-    """
-    An extended SelectMultiple widget that renders a combobox.
-    """
-    template_name = "widgets/combobox.html"
-    checked_attribute = {"checked": True}
+class RenderMixin:
+    """Render mixin."""
 
     def render(self, name, value, attrs=..., renderer=...):
         context = self.get_context(name, value, attrs)
         return render_to_string(self.template_name, context)
 
 
-class OrderByWidget(SelectMultiple):
+class AutocompleteWidget(RenderMixin, TextInput):
+    """
+    An extended TextInput widget that renders an autocomplete.
+    """
+
+    template_name = "widgets/autocomplete.html"
+
+
+class ComboboxWidget(RenderMixin, SelectMultiple):
+    """
+    An extended SelectMultiple widget that renders a combobox.
+    """
+
+    template_name = "widgets/combobox.html"
+    checked_attribute = {"checked": True}
+
+
+class OrderByWidget(RenderMixin, SelectMultiple):
     """
     An extended SelectMultiple widget that order functionality and styling.
     """
 
     template_name = "widgets/order_by.html"
 
-    def render(self, name, value, attrs=..., renderer=...):
-        context = self.get_context(name, value, attrs)
-        return render_to_string(self.template_name, context)
+
+def get_autocomplete_field(**kwargs: dict[str, str]) -> forms.CharField:
+    """Get autocomplete field."""
+    return forms.CharField(
+        widget=AutocompleteWidget({"data-data": json.dumps(kwargs)}),
+    )
