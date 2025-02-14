c2b98bd3325115bec4e38c3a3b6cb13cc260737e
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-02-14 11:42:37 +0200

    add more test cases

--- tests/geo/nationalities/_conftest/test_cases.py
+++ tests/geo/nationalities/_conftest/test_cases.py
@@ -39,6 +39,13 @@ def pagination_test_cases(request: pytest.FixtureRequest):
             ("Nationality 003", "Nationality 004"),
             ("Nationality 001", "Nationality 002"),
         ),
+        (
+            '?q=name startswith "Nationality 00"',
+            9,
+            "name",
+            [f"Nationality 00{idx}" for idx in range(1, 10)],
+            ("Nationality 100", "Nationality 099"),
+        ),
     ],
 )
 def filters_test_cases(request: pytest.FixtureRequest):
@@ -51,6 +58,7 @@ def filters_test_cases(request: pytest.FixtureRequest):
         ("?ordering=Id", (1, 2, 10)),
         ("?ordering=-Id", (100, 99, 91)),
         ("?ordering=-Locality", (99, 1, 9)),
+        ("?ordering=Locality", (1, 2, 10)),
     ],
 )
 def order_test_cases(request: pytest.FixtureRequest):
@@ -102,8 +110,44 @@ def export_test_cases(request: pytest.FixtureRequest):
             ["Ensure this value has at most 255 characters (it has 265)."],
         ),
         (
-            {"name": "Nationality 100", "is_local": True, "description": "google"},
+            {
+                "name": "Nationality 100",
+                "is_local": True,
+                "description": "google",
+            },
             ["Nationality with this Name already exists."],
+        ),
+        (
+            {
+                "name": "Nationality 100",
+                "is_local": "sss",
+                "description": "google",
+            },
+            [
+                "Nationality with this Name already exists.",
+                "Select a valid choice. sss is not one of the available choices.",
+            ],
+        ),
+        (
+            {
+                "name": "Nationality 1312",
+                "is_local": 4,
+                "description": "google",
+            },
+            [
+                "Select a valid choice. 4 is not one of the available choices.",
+            ],
+        ),
+        (
+            {
+                "name": "Na",
+                "is_local": 4,
+                "description": "google",
+            },
+            [
+                "the field must be at least 4 characters long.",
+                "Select a valid choice. 4 is not one of the available choices.",
+            ],
         ),
     ],
 )
@@ -129,6 +173,21 @@ def dirty_data_test_cases(request: pytest.FixtureRequest):
         (
             {"name": "Nationality 001", "description": "google"},
             {"name": ["nationality with this name already exists."]},
+        ),
+        (
+            {"name": "Nationality 3131", "is_local": "3123", "description": "google"},
+            {"is_local": ["Must be a valid boolean."]},
+        ),
+        (
+            {"name": "Nationality 3131", "is_local": 3123, "description": "google"},
+            {"is_local": ["Must be a valid boolean."]},
+        ),
+        (
+            {"name": "Na", "is_local": 3123, "description": "google"},
+            {
+                "name": ["Ensure this field has at least 4 characters."],
+                "is_local": ["Must be a valid boolean."],
+            },
         ),
     ],
 )
