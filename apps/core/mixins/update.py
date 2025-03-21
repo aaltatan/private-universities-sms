340153086a02bd4b6740c79fa6df6e3a566757d6
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-03-21 21:04:31 +0200

    fix ValueError: The Model could not be created because the data didn't validate. when delete empty formset

--- apps/core/mixins/update.py
+++ apps/core/mixins/update.py
@@ -125,10 +125,9 @@ class UpdateMixin(ABC):
         obj = form.save()
         for formset in formsets:
             for form in formset.forms:
-                if form.cleaned_data:
+                if form.is_valid():
                     item = form.save(commit=False)
                     item.ordering = form.cleaned_data.get("ORDER") or 1_000_000
-                    item.save()
             formset.save()
 
         messages.success(
