da0e1eb56eb23616c493446f9d61ac9cad30b151
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2025-05-28 12:04:05 +0300

    replacing flatten sidebar items with groups

--- project/constants.py
+++ project/constants.py
@@ -5,17 +5,8 @@ from django.http import HttpRequest
 from django.utils.translation import gettext as _
 from django.urls import reverse
 
-from apps.core.utils import get_apps_links
-from apps.core.schemas import AppLink
-
 
 def constants(request: HttpRequest) -> dict[str, Any]:
-    dashboard_link = AppLink(
-        icon="chart-pie",
-        text=_("dashboard"),
-        path=reverse("core:dashboard"),
-    )
-
     data = {
         "project_name": "Salaries Management System",
         "settings": {
@@ -23,15 +14,95 @@ def constants(request: HttpRequest) -> dict[str, Any]:
             "per_page_array": settings.PER_PAGE_ARRAY,
             "per_page": settings.PER_PAGE,
         },
-        "sidebar_links": get_apps_links(
-            request=request,
-            additional_links=[dashboard_link],
-            unlinked_models=[
-                "employee_groups",
-                "email",
-                "phone",
-                "mobile",
-            ],
-        ),
+        "sidebar_links": {
+            "core": {
+                "dashboard": {
+                    "text": _("dashboard").title(),
+                    "path": reverse("core:dashboard"),
+                },
+            },
+            "geo": {
+                "governorates": {
+                    "text": _("governorates").title(),
+                    "path": reverse("geo:governorates:index"),
+                },
+                "cities": {
+                    "text": _("cities").title(),
+                    "path": reverse("geo:cities:index"),
+                },
+                "nationalities": {
+                    "text": _("nationalities").title(),
+                    "path": reverse("geo:nationalities:index"),
+                },
+            },
+            "org": {
+                "cost_centers": {
+                    "text": _("cost centers").title(),
+                    "path": reverse("org:cost_centers:index"),
+                },
+                "job_types": {
+                    "text": _("job types").title(),
+                    "path": reverse("org:job_types:index"),
+                },
+                "job_subtypes": {
+                    "text": _("job subtypes").title(),
+                    "path": reverse("org:job_subtypes:index"),
+                },
+                "groups": {
+                    "text": _("groups").title(),
+                    "path": reverse("org:groups:index"),
+                },
+                "positions": {
+                    "text": _("positions").title(),
+                    "path": reverse("org:positions:index"),
+                },
+                "statuses": {
+                    "text": _("statuses").title(),
+                    "path": reverse("org:statuses:index"),
+                },
+            },
+            "edu": {
+                "school_kinds": {
+                    "text": _("school kinds").title(),
+                    "path": reverse("edu:school_kinds:index"),
+                },
+                "schools": {
+                    "text": _("schools").title(),
+                    "path": reverse("edu:schools:index"),
+                },
+                "degrees": {
+                    "text": _("degrees").title(),
+                    "path": reverse("edu:degrees:index"),
+                },
+                "specializations": {
+                    "text": _("specializations").title(),
+                    "path": reverse("edu:specializations:index"),
+                },
+            },
+            "fin": {
+                "years": {
+                    "text": _("years").title(),
+                    "path": reverse("fin:years:index"),
+                },
+                "periods": {
+                    "text": _("periods").title(),
+                    "path": reverse("fin:periods:index"),
+                },
+                "taxes": {
+                    "text": _("taxes").title(),
+                    "path": reverse("fin:taxes:index"),
+                },
+                "tax_brackets": {
+                    "text": _("tax brackets").title(),
+                    "path": reverse("fin:tax_brackets:index"),
+                },
+            },
+            "hr": {
+                "employees": {
+                    "text": _("employees").title(),
+                    "path": reverse("hr:employees:index"),
+                },
+            },
+        },
     }
     return data
