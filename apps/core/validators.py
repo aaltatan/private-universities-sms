628ab4a59b4e45cf7346d582684ca22d5e24a0d8
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-06-07 15:18:52 +0300

    make validate_file_mimetype for more generic file extensions

--- apps/core/validators.py
+++ apps/core/validators.py
@@ -1,10 +1,13 @@
+from typing import Any, Callable
+
+import magic
+from django.core.exceptions import ValidationError
 from django.core.validators import (
+    FileExtensionValidator,
     MinLengthValidator,
     RegexValidator,
-    FileExtensionValidator,
 )
 from django.utils.translation import gettext_lazy as _
-
 
 four_char_length_validator = MinLengthValidator(
     limit_value=4,
@@ -31,7 +34,34 @@ syrian_phone_validator = RegexValidator(
     message=_("the field must be syrian phone number like 0332756651."),
 )
 
-document_extension_validator = FileExtensionValidator(
+pdf_image_extension_validator = FileExtensionValidator(
     allowed_extensions=["pdf", "png", "jpg", "jpeg"],
     message=_("the field must be a valid pdf, png, jpg or jpeg file."),
 )
+
+image_extension_validator = FileExtensionValidator(
+    allowed_extensions=["jpg", "jpeg", "png"],
+    message=_("the field must be a valid jpg, jpeg or png file."),
+)
+
+
+def validate_file_mimetype(
+    accepted_mimetypes: list[str], message: str = _("Unsupported file type.")
+) -> Callable[[Any], None]:
+    """
+    Validates the file mimetype.
+
+    Args:
+        accepted_mimetypes (list[str]): The list of accepted mimetypes.
+        message (str): The error message.
+
+    Returns:
+        Callable[[Any], None]: The validation function.
+    """
+
+    def _validate_file_mimetype(file) -> None:
+        file_mimetype = magic.from_buffer(file.read(1024), mime=True)
+        if file_mimetype not in accepted_mimetypes:
+            raise ValidationError(message)
+
+    return _validate_file_mimetype
