c5070b14bb53029e1d8a6b60013f89d2a2802e1a
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-06-13 20:23:13 +0300

    some enhancements

--- apps/core/templatetags/formats.py
+++ apps/core/templatetags/formats.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import Literal
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -6,23 +7,27 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-def format_string(
+def _format_string(
     value: str,
     chars_width: int = 15,
     in_table: bool = False,
+    color: Literal["red", "green", "none"] = "none",
 ) -> str:
     right_result = value.rjust(chars_width)
     left_result = value.ljust(chars_width)
+
+    text_color = "" if color == "none" else f"style='color: {color}'"
+
     if in_table:
         return mark_safe(
             f"""
-            <pre class='rtl:hidden @max-lg:hidden font-medium'>{right_result}</pre>
-            <pre class='ltr:hidden @max-lg:hidden font-medium'>{left_result}</pre>
-            <pre class='@lg:hidden font-medium'>{value}</pre>
+            <pre {text_color} class='rtl:hidden @max-lg:hidden font-medium'>{right_result}</pre>
+            <pre {text_color} class='ltr:hidden @max-lg:hidden font-medium'>{left_result}</pre>
+            <pre {text_color} class='@lg:hidden font-medium'>{value}</pre>
             """,
         )
     else:
-        return mark_safe(f"<pre class='font-medium'>{value}</pre>")
+        return mark_safe(f"<pre {text_color} class='font-medium'>{value}</pre>")
 
 
 @register.filter
@@ -32,14 +37,28 @@ def percentage(
     decimal_places: int = 2,
 ) -> str:
     result = f"{value * 100:.{decimal_places}f} %"
-    return format_string(result, in_table=in_table, chars_width=10)
+    return _format_string(result, in_table=in_table, chars_width=10)
 
 
 @register.filter
 def money(
-    value: float | Decimal | int,
+    value: float | Decimal | int | str,
     in_table: bool = False,
+    decimal_places: int = 2,
     chars_width: int = 15,
 ) -> str:
-    result = f"{value:,}"
-    return format_string(result, in_table=in_table, chars_width=chars_width)
+    if value == "":
+        return "0"
+
+    if not isinstance(value, str):
+        if value < 0:
+            value = abs(value)
+            result = f"({value:,.{decimal_places}f})"
+        else:
+            result = f"{value:,.{decimal_places}f}"
+
+    return _format_string(
+        result,
+        in_table=in_table,
+        chars_width=chars_width,
+    )
