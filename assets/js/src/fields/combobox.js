ca1b478bf7ddc066923c0875b198c2445b040b75
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-06-28 01:53:33 +0300

    fix search issue

--- assets/js/src/fields/combobox.js
+++ assets/js/src/fields/combobox.js
@@ -54,8 +54,8 @@ export function combobox(comboboxData = { options: [], selectedText: "" }) {
 
       Alpine.$data(container).options = Alpine.$data(container).options.map((e) => {
         e.show = false;
-        let searchName = e.label + " " + e.label;
-        let pattern = new RegExp(keywords.split(" ").join(".+"), "g");
+        let searchName = e.label.toLowerCase() + " " + e.label.toLowerCase();
+        let pattern = new RegExp(keywords.toLowerCase().split(" ").join(".+"), "g");
         if (searchName.match(pattern)) {
           e.show = true;
         }
