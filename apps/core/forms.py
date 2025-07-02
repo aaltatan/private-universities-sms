6ee0118558d0013d15dd18ee6f12cca8f713ca47
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-07-02 09:07:53 +0300

    some fixes

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -13,17 +13,11 @@ class CustomModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        autocomplete_fields = [
-            {field_name: field}
-            for field_name, field in self.fields.items()
-            if type(field) == CustomModelChoiceField
-        ]
+        for field_name, field in self.fields.items():
+            if type(field) == CustomModelChoiceField:
+                related_instance = getattr(self.instance, field_name, None)
 
-        for field in autocomplete_fields:
-            field_name, field = list(field.items())[0]
-            related_instance = getattr(self.instance, field_name, None)
-
-            if self.instance and related_instance:
-                self.initial[field_name] = getattr(
-                    related_instance, field.to_field_name
-                )
+                if self.instance and related_instance:
+                    self.initial[field_name] = getattr(
+                        related_instance, field.to_field_name
+                    )
