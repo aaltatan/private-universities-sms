4f95665d22645240a1100de942fd842a9d6c1ed9
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2025-07-05 11:38:54 +0300

    add get_initial_queryset method instead of queryset attr

--- apps/trans/views/voucher_transactions.py
+++ apps/trans/views/voucher_transactions.py
@@ -22,9 +22,11 @@ class ListView(
     resource_class = resources.VoucherTransactionResource
     deleter = VoucherDeleter
     ordering_fields = constants.ORDERING_FIELDS
-    queryset = models.VoucherTransaction.objects.filter(
-        voucher__is_deleted=False,
-    ).order_by("-voucher__voucher_serial", "voucher__date", "ordering")
+
+    def get_initial_queryset(self):
+        return models.VoucherTransaction.objects.filter(
+            voucher__is_deleted=False,
+        )
 
     def get_actions(self):
         return {}
