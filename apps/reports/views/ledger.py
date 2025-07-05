b2ea7965a85875366e1c3509d7abf6267ff1d00a
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-07-05 19:15:02 +0300

    use ListMixin

--- apps/reports/views/ledger.py
+++ apps/reports/views/ledger.py
@@ -1,73 +1,46 @@
-from django.http import HttpRequest
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.urls import resolve, reverse
 from django.views.generic import View
+from django.views.generic.list import MultipleObjectMixin
 
 from apps.core import mixins
 from apps.hr.models import Employee
 from apps.trans.filters import LedgerFilter
 from apps.trans.models import JournalEntry
 
-# from ..resources import LedgerResource
+from .. import resources
 
 
 class LedgerView(
-    mixins.PaginationMixin,
-    mixins.TemplateVariablesMixin,
-    mixins.SearchFilterMixin,
-    mixins.TemplateNamesMixin,
-    mixins.SidebarFilterMixin,
+    PermissionRequiredMixin,
+    mixins.ListMixin,
+    MultipleObjectMixin,
     View,
 ):
-    model = JournalEntry
     app_label = "reports"
     verbose_name_plural = "ledger"
+    permission_required = "trans.view_journalentry"
+    model = JournalEntry
     filter_class = LedgerFilter
+    resource_class = resources.LedgerResource
+    order_filter = False
 
-    def get(self, request: HttpRequest, slug: str, *args, **kwargs):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["employee"] = self.employee
+        return context
+
+    def get_initial_queryset(self):
+        resolved = resolve(self.request.path_info)
+        slug = resolved.kwargs.get("slug")
+
         self.employee = get_object_or_404(Employee, slug=slug)
 
-        queryset = JournalEntry.objects.filter(employee=self.employee)
-
-        filter_obj = LedgerFilter(request.GET, queryset, request=request)
-        queryset = filter_obj.qs
-
-        SearchFilter = self.get_search_filter_class()
-        search_filter = SearchFilter(request.GET, queryset=queryset, request=request)
-        queryset = search_filter.qs
-
-        if request.GET.get("filters"):
-            template_name = self.get_filter_form_template_name()
-            context = self.sidebar_filters_context_data(request, queryset)
-            return self.get_filters_response(request, template_name, context)
-
-        page = self.get_page_class(request, queryset)
-
-        if request.htmx:
-            template_name = self.get_table_template_name()
-        else:
-            template_name = self.get_template_name()
-
-        context = {
-            "employee": self.employee,
-            "page": page,
-            "search_filter": search_filter,
-            "app_label": "reports",
-            "subapp_label": "ledger",
-            "model_name": JournalEntry._meta.model_name,
-            "model": JournalEntry,
-            **self.get_create_url(),
-            **self.get_index_url(),
-            **self.get_html_ids(),
-            **self.get_permissions(request),
-        }
-
-        return render(request, template_name, context)
+        return JournalEntry.objects.filter(employee=self.employee)
 
     def get_index_url(self):
-        return {
-            "index_url": reverse(
-                f"{self.app_label}:{self.verbose_name_plural}",
-                kwargs={"slug": self.employee.slug},
-            )
-        }
+        return reverse("reports:ledger", kwargs={"slug": self.employee.slug})
+
+    def get_dataset_title(self):
+        return self.employee.fullname
